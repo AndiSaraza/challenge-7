cb967e4bb872db5e04eb90185912da0fcc0b2ec0
AndiSaraza <148919690+AndiSaraza@users.noreply.github.com>
2023-12-31 16:02:26 -0300

    Add files via upload

---
+++ Challenge 7/js/areas.js
@@ -0,0 +1,19 @@
+console.log('CALCULOS');
+
+console.log('Area de un triangulo');
+
+let base, altura;
+base=prompt('Ingrese la base del triangulo');
+altura=prompt('Ingrese el altura del triangulo')
+
+let triangulo=(base*altura/2);
+console.log(triangulo);
+
+console.log('Area de un circulo');
+
+let radio;
+var pi= 3.14;
+radio=prompt('Ingrese el radio del triangulo')
+
+let circulo=(pi*(Math.pow(radio, 2)));
+console.log(circulo);
